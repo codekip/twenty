34a8ae45903cd9fc2983d915fe1247631ebaaf48
SkelmDev <skelmdev@gmail.com>
2020-02-04 13:18:26 +0000

    Context manager did not work.Now to add separate delete function to del files once modified

--- pdfcrop.py
+++ pdfcrop.py
@@ -6,54 +6,32 @@ from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 PATH = r"L:\01 Site Tools\02 Operational Tools\62 DSV\**" + os.sep
 OUTPUT_PATH = r"L:\01 Site Tools\02 Operational Tools\62 DSV" + os.sep
 FILE_EXTENSION = "*.pdf"
+IGNORE = ("resize")
 
 def get_files():
-    files = glob.glob(PATH + FILE_EXTENSION,recursive=True)
-
-    get_dimensions(files)
-
-    for file in files:
-        try:
-            os.remove(file)
-        except:
-            pass
+    all_files = glob.glob(PATH + FILE_EXTENSION,recursive=True)
+    files = [file for file in all_files if not IGNORE in file]
+   
+    modify_file(files)
     
-def get_dimensions(files):
+def modify_file(files):
     count = 0
     for file in files:
-        #print("FILENAME IS", file)
         pdf_file = PdfFileReader(open(file,"rb"))
-        #page = pdf_file.getPage(0)
-        # print("lower left",page.cropBox.getLowerLeft())
-        # print("lower right",page.cropBox.getLowerRight())
-        # print("upper left",page.cropBox.getUpperLeft())
-        # print("upper right",page.cropBox.getUpperRight())
-        # print("Mid point",page.cropBox.getUpperRight())
-        #print(pdf_file.getDocumentInfo())
-
-
         crop(pdf_file, count)
         count += 1
 
 def crop(pdf, count):
     output = PdfFileWriter()
     pagecount = pdf.getNumPages()
-    #print(f"The document has {pagecount} pages ")    
 
     for i in range(pagecount):
         page = pdf.getPage(i)
         upper_right = page.mediaBox.getUpperRight_x(),page.mediaBox.getUpperRight_y()
-        #upper_left = page.mediaBox.getUpperLeft_x(), page.mediaBox.getUpperLeft_y()
         mid_point_left = 0, page.mediaBox.getUpperLeft_y()/2
-        #mid_point_right = page.mediaBox.getLowerRight_x(), page.mediaBox.getUpperRight_y()/2
         
         page.cropBox.lowerLeft = mid_point_left
         page.cropBox.upperRight = upper_right
-
-        #print("Media box upper right ",upper_right)
-        #print("Media box upper left ",upper_left)
-        #print(mid_point_left)
-        #print(mid_point_right)
         page.rotateCounterClockwise(90)
         output.addPage(page)
 
